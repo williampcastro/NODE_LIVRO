71b3e9f4bc00580fa2b805740cd183121ebf9e7a
williamthbeblack <williampinheiro@gec.inatel.br>
2018-08-25 08:26:44 -0300

    Update buffer 2.5

--- CAP_2/buffer2.5.js
+++ CAP_2/buffer2.5.js
@@ -1,20 +1,21 @@
-var fib = (n) =>{
-    if(n<2) return n;
-    return fib(n-1) + fib(n+2);
+var fib = function (n) {
+    if (n < 2) return n;
+    return fib(n - 1) + fib(n - 2);
 };
 
-var Obj = function() {};
+var Obj = function () { };
 
-Obj.prototype.doSomething = (arg1_) => {
-    var callback_ = arguments[arguments.lenth-1];
-    callback = (typeof(callback_) == 'function' ? callback_ : null);
+Obj.prototype.doSomething = function (arg1_) {
+    var callback_ = arguments[arguments.length - 1];
+    callback = (typeof (callback_) == 'function' ? callback_ : null);
     var arg1 = typeof arg1_ === 'number' ? arg1_ : null;
 
-    if(!arg1)
+    if (!arg1)
         return callback(new Error('first arg missing or not a number'));
 
-    process.nextTick(()=>{
-        //bloqueia CPU
+    process.nextTick(function () {
+
+        // block on CPU
         var data = fib(arg1);
         callback(null, data);
     });
@@ -23,11 +24,11 @@ Obj.prototype.doSomething = (arg1_) => {
 var test = new Obj();
 var number = 10;
 
-test.doSomething(number, (err, value) => {
-    if (err) 
-        console.err(err);
-    else 
-        console.log('fibonaci value ofr %d is %d, number, value');
+test.doSomething(number, function (err, value) {
+    if (err)
+        console.error(err);
+    else
+        console.log('fibonaci value for %d is %d', number, value);
 });
 
-console.log('called doSomething')
+console.log('called doSomething');
